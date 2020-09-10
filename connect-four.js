e2a3e792bf5743324963b69f54b8a2d8e8073843
salina kuo <salina.kuo97@gmail.com>
2020-09-10 17:21:03 -0400

    token hover colors

--- connect-four.js
+++ connect-four.js
@@ -1,19 +1,25 @@
-import { Game } from './game.js'; 
+import { Game } from './game.js';
+const clickTarget = document.getElementById("click-targets")
 
 let game = undefined;
+
 function updateUI() {
     if (game === undefined){
         document.getElementById("board-holder").classList.add("is-invisible");
-    } else {
+    } else if (game !== undefined) {
         document.getElementById("board-holder").classList.remove("is-invisible");
         document.getElementById("game-name").innerHTML = game.getName();
+        if (game.playerTracker === 1) {
+            clickTarget.classList.remove('red');
+            clickTarget.classList.add('black')
+        } else if (game.playerTracker === 2) {
+            clickTarget.classList.remove('black')
+            clickTarget.classList.add('red')
+        }
     }
-
 }
+
 window.addEventListener("DOMContentLoaded", event => {
-
-
-
 
 let player1Input = document.getElementById("player-1-name");
 let player2Input = document.getElementById("player-2-name");
@@ -40,7 +46,10 @@ buttonVariable.addEventListener("click", (event) => {
     updateUI();
 });
 
-
+document.getElementById("click-targets").addEventListener("click", (event) => {
+    game.playInColumn();
+    updateUI();
+})
 
 
 
--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 export class Game {
-    constructor(name1, name2, gameName) {
+    constructor(name1, name2) {
         this.name1 = name1;
         this.name2 = name2;
-        this.gameName = gameName;
+        this.playerTracker = 1;
     }
 
     getName(){
         return `${this.name1} vs. ${this.name2}`;
+    }
+
+    playInColumn() {
+        if (this.playerTracker === 1){
+            this.playerTracker = 2;
+        } else {
+            this.playerTracker = 1;
+        }
     }
 }
